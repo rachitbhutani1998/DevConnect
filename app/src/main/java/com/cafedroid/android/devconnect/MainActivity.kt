017c8b1ddac3cf74c42a76b1d3f71a49e22c1d0d
rachit <rachitbhutani1998@gmail.com>
2018-10-23 11:57:13 +0530

    fixed theme settings

--- app/src/main/java/com/cafedroid/android/devconnect/MainActivity.kt
+++ app/src/main/java/com/cafedroid/android/devconnect/MainActivity.kt
@@ -3,40 +3,64 @@ package com.cafedroid.android.devconnect
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBar
+import android.support.v7.widget.Toolbar
 import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var menu: Menu
+    private lateinit var mDrawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
-        val mDrawerLayout: DrawerLayout = findViewById(R.id.main_drawer)
-        val mNavigationView:NavigationView=findViewById(R.id.nav_view)
+        val actionbar: ActionBar? = supportActionBar
+        actionbar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp)
+        }
+
+        mDrawerLayout = findViewById(R.id.main_drawer)
+        val mNavigationView: NavigationView = findViewById(R.id.nav_view)
 
         menu = mNavigationView.menu
-        menu.add(12,0,0,"+ Add a team")
-        menu.setGroupCheckable(12,false,true)
-        menu.add(123,1,Menu.NONE,"First Team")
+        menu.add(12, 0, 0, "+ Add a team")
+        menu.setGroupCheckable(12, false, true)
+        menu.add(123, 1, Menu.NONE, "First Team")
 
         mNavigationView.setNavigationItemSelectedListener { menuItem ->
-            Log.e("MainActivity",menuItem.title.toString() + " " + menuItem.itemId)
+            Log.e("MainActivity", menuItem.title.toString() + " " + menuItem.itemId)
             menuItem.isChecked = true
-            when(menuItem.title){
-                "+ Add a team"->{
-                    menu.add(123,1,Menu.NONE,"Random Team")
-                    menu.setGroupCheckable(123,true,true)
+            when (menuItem.title) {
+                "+ Add a team" -> {
+                    menu.add(123, 1, Menu.NONE, "Random Team")
                 }
-                else ->{
-
+                else -> {
+                    menu.setGroupCheckable(123, true, true)
                 }
             }
             mDrawerLayout.closeDrawers()
             true
+        }
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                mDrawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
