63e0cf62d36bf767546a65ab2bb0fa95d8e7412d
rachit <rachitbhutani1998@gmail.com>
2018-10-25 16:19:44 +0530

    added auth/problem in redirecting

--- app/src/main/java/com/cafedroid/android/devconnect/AuthActivity.kt
+++ app/src/main/java/com/cafedroid/android/devconnect/AuthActivity.kt
@@ -1,12 +1,37 @@
 package com.cafedroid.android.devconnect
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageButton
 
 class AuthActivity : AppCompatActivity() {
+
+    val clientId:String="3641e84228dcf2c013f7"
+    val clientSecret:String="5b80b58068b439ce2c3ab86e0e8ee9f317ecd008"
+    val redirectURL:String="localhost:3000/auth/github"
+    val BASE_URL:String="https://github.com/login/oauth/authorize"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+
+        val githubSignInBtn: ImageButton =findViewById(R.id.github_signin_btn)
+        val githubClickListener:View.OnClickListener= View.OnClickListener { view ->
+            val uri:Uri=Uri.parse(BASE_URL).buildUpon()
+                .appendQueryParameter("client_id",clientId).build()
+            startActivity(Intent(Intent.ACTION_VIEW,uri))
+        }
+        githubSignInBtn.setOnClickListener(githubClickListener)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val uri=intent.data
+        if (uri!=null)
+            Log.e("This Activity",uri.toString())
     }
 }
--- app/src/main/java/com/cafedroid/android/devconnect/MainActivity.kt
+++ app/src/main/java/com/cafedroid/android/devconnect/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.cafedroid.android.devconnect
 
+import android.app.PendingIntent.getActivity
+import android.content.Context
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.NavigationView
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentTransaction
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBar
@@ -10,15 +15,29 @@ import android.support.v7.widget.Toolbar
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.content.Context.MODE_PRIVATE
+import android.content.Intent
+
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var menu: Menu
     private lateinit var mDrawerLayout: DrawerLayout
+    lateinit var mNavigationView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sharedPref = this.getPreferences(Context.MODE_PRIVATE) ?: return
+        val authTokenString:String=sharedPref.getString("AuthToken","Unavailable")
+        if (authTokenString == "Unavailable"){
+            startActivity(Intent(this,AuthActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+            finish()
+        }
+
+
+
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
@@ -29,24 +48,31 @@ class MainActivity : AppCompatActivity() {
         }
 
         mDrawerLayout = findViewById(R.id.main_drawer)
-        val mNavigationView: NavigationView = findViewById(R.id.nav_view)
+        mNavigationView = findViewById(R.id.nav_view)
 
         menu = mNavigationView.menu
-        menu.add(12, 0, 0, "+ Add a team")
-        menu.setGroupCheckable(12, false, true)
-        menu.add(123, 1, Menu.NONE, "First Team")
+        menu.add(123, 0, 0, "+ Add a team")
 
         mNavigationView.setNavigationItemSelectedListener { menuItem ->
             Log.e("MainActivity", menuItem.title.toString() + " " + menuItem.itemId)
             menuItem.isChecked = true
             when (menuItem.title) {
                 "+ Add a team" -> {
-                    menu.add(123, 1, Menu.NONE, "Random Team")
-                }
-                else -> {
+                    val fragTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+                    val prev: Fragment? = supportFragmentManager.findFragmentByTag("dialog")
+                    if (prev != null) {
+                        fragTransaction.remove(prev)
+                    }
+                    fragTransaction.addToBackStack(null)
+                    fragTransaction.add(R.id.container, AddTeamDialog(), "dialog").commit()
+                    mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+//                    menu.add(123, 1, Menu.NONE, "Random Team")
                     menu.setGroupCheckable(123, true, true)
                 }
+                else -> {
+                }
             }
+            menu.setGroupCheckable(123, true, true)
             mDrawerLayout.closeDrawers()
             true
         }
@@ -61,6 +87,19 @@ class MainActivity : AppCompatActivity() {
                 true
             }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(mNavigationView)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+            val size = mNavigationView.menu.size()
+            for (i in 0 until size) {
+                mNavigationView.menu.getItem(i).isChecked = false
+            }
+            mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
         }
     }
 }
