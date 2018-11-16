ad505f733ec612df8ef2b02769ca3af27decc63f
rachit <rachitbhutani1998@gmail.com>
2018-11-17 02:53:11 +0530

    fetching links from browsers

--- app/src/main/java/com/cafedroid/android/devconnect/MainActivity.kt
+++ app/src/main/java/com/cafedroid/android/devconnect/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.cafedroid.android.devconnect
 
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
@@ -9,6 +10,7 @@ import android.support.v4.app.Fragment
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBar
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.util.Base64
@@ -16,10 +18,7 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ImageView
-import android.widget.ListView
-import android.widget.TextView
+import android.widget.*
 import com.androidnetworking.AndroidNetworking
 import com.bumptech.glide.Glide
 import com.cafedroid.android.devconnect.classes.Users
@@ -34,7 +33,7 @@ import org.json.JSONObject
 import java.io.UnsupportedEncodingException
 
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
 
     lateinit var menu: Menu
     lateinit var mDrawerLayout: DrawerLayout
@@ -45,12 +44,12 @@ class MainActivity : AppCompatActivity(){
     private val chatFragment: Fragment = ChatFragment()
 
     private lateinit var onlineUserList: ArrayList<User>
-    lateinit var roomsList:ArrayList<Room>
+    lateinit var roomsList: ArrayList<Room>
 
     private lateinit var onlineAdapter: OnlineListAdapter
-    lateinit var roomListAdapter:RoomListAdapter
+    lateinit var roomListAdapter: RoomListAdapter
 
-    var currentRoom:Room?=null
+    var currentRoom: Room? = null
     lateinit var chatKitUser: CurrentUser
     lateinit var sharedPref: SharedPreferences
     lateinit var USER_ID: String
@@ -60,11 +59,11 @@ class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.e("Main","started")
+        Log.e("Main", "started")
         supportFragmentManager.beginTransaction().add(R.id.container, splashFragment).commit()
         mDrawerLayout = findViewById(R.id.main_drawer)
         mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
-        Log.e("Main","splashed")
+        Log.e("Main", "splashed")
         AndroidNetworking.initialize(applicationContext)
 
         mNavigationView = findViewById(R.id.nav_view)
@@ -73,41 +72,39 @@ class MainActivity : AppCompatActivity(){
         if (authTokenString == "Unavailable") {
             startActivity(Intent(this, AuthActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
             finish()
-        }
+        } else {
 
-        val jsonObject = JSONObject(decoded(authTokenString))
-        val currentUsers = Users(
-            jsonObject.optString("id")
-            , jsonObject.optString("name")
-            , jsonObject.optString("avatar")
-        )
+            val jsonObject = JSONObject(decoded(authTokenString))
+            val currentUsers = Users(
+                jsonObject.optString("id")
+                , jsonObject.optString("name")
+                , jsonObject.optString("avatar")
+            )
 
-        USER_ID = currentUsers.userName
-
-
+            USER_ID = currentUsers.userName
 
 
-        val onlineUserListView: ListView = findViewById(R.id.online_user_list)
-        onlineUserListView.emptyView = findViewById(R.id.empty_view_users)
-        onlineUserList = ArrayList()
+            val onlineUserListView: ListView = findViewById(R.id.online_user_list)
+            onlineUserListView.emptyView = findViewById(R.id.empty_view_users)
+            onlineUserList = ArrayList()
 
-        val roomsListView:ListView=findViewById(R.id.rooms_nav_lv)
-        roomsList= ArrayList()
-        roomListAdapter= RoomListAdapter(this,roomsList)
-        roomsListView.adapter=roomListAdapter
+            val roomsListView: ListView = findViewById(R.id.rooms_nav_lv)
+            roomsList = ArrayList()
+            roomListAdapter = RoomListAdapter(this, roomsList)
+            roomsListView.adapter = roomListAdapter
 
-        onlineAdapter = OnlineListAdapter(this, onlineUserList)
-        onlineUserListView.adapter = onlineAdapter
+            onlineAdapter = OnlineListAdapter(this, onlineUserList)
+            onlineUserListView.adapter = onlineAdapter
 
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
+            val toolbar: Toolbar = findViewById(R.id.toolbar)
+            setSupportActionBar(toolbar)
 
-        val actionbar: ActionBar? = supportActionBar
-        actionbar?.apply {
-            setDisplayHomeAsUpEnabled(true)
-            setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp)
-        }
-        actionbar!!.title = "DevConnect"
+            val actionbar: ActionBar? = supportActionBar
+            actionbar?.apply {
+                setDisplayHomeAsUpEnabled(true)
+                setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp)
+            }
+            actionbar!!.title = "DevConnect"
 
 
 //        menu = mNavigationView.menu
@@ -139,41 +136,66 @@ class MainActivity : AppCompatActivity(){
 //            true
 //        }
 
-        val header:View=mNavigationView.getHeaderView(0)
-        val profileImageView:ImageView=header.findViewById(R.id.user_profile_image)
-        val profileNameView:TextView=header.findViewById(R.id.user_profile_name)
-        Glide.with(this).load(currentUsers.userImage).into(profileImageView)
-        profileNameView.text=currentUsers.fullName
-        val chatManager = ChatManager(
-            instanceLocator = INSTANCE_LOCATOR,
-            userId = USER_ID,
-            dependencies = AndroidChatkitDependencies(
-                tokenProvider = ChatkitTokenProvider(
-                    endpoint = "https://ancient-temple-53657.herokuapp.com/api/auth/authenticate",
-                    userId = USER_ID
+            val header: View = mNavigationView.getHeaderView(0)
+            val profileImageView: ImageView = header.findViewById(R.id.user_profile_image)
+            val profileNameView: TextView = header.findViewById(R.id.user_profile_name)
+            val signOutBtn:ImageButton=header.findViewById(R.id.sign_out_btn)
+            signOutBtn.setOnClickListener{
+
+                val alert=AlertDialog.Builder(this)
+                    .setTitle("Log Out?")
+                    .setMessage("Are you sure you want to log out?")
+                    .setPositiveButton("I don't want to work") { dialogInterface, i ->
+                        val sharedPrefEditor=sharedPref.edit()
+                        with(sharedPrefEditor){
+                            putString("auth_token","Unavailable")
+                            apply()
+                        }
+                        val token: String = sharedPref.getString("auth_token", "Unavailable")
+                        if (token == "Unavailable") {
+                            startActivity(Intent(this, AuthActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+                            finish()
+                        }
+                    }.setNegativeButton("Keep Contributing"){dialogInterface, i ->
+                        dialogInterface.dismiss()
+                    }.create()
+                alert.show()
+            }
+
+            Glide.with(this).load(currentUsers.userImage).into(profileImageView)
+            profileNameView.text = currentUsers.fullName
+            val chatManager = ChatManager(
+                instanceLocator = INSTANCE_LOCATOR,
+                userId = USER_ID,
+                dependencies = AndroidChatkitDependencies(
+                    tokenProvider = ChatkitTokenProvider(
+                        endpoint = "https://ancient-temple-53657.herokuapp.com/api/auth/authenticate",
+                        userId = USER_ID
+                    )
                 )
             )
-        )
-        chatManager.connect { result ->
-            when (result) {
-                is Result.Success -> {
-                    chatKitUser=result.value
-                    supportFragmentManager.beginTransaction().remove(splashFragment).add(R.id.container, chatFragment).commit()
-                    mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            chatManager.connect { result ->
+                when (result) {
+                    is Result.Success -> {
+                        chatKitUser = result.value
+                        supportFragmentManager.beginTransaction().remove(splashFragment)
+                            .add(R.id.container, chatFragment).commit()
+                        mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+                    }
+                    is Result.Failure -> Log.e("ActivityDC", result.error.reason)
+                    else -> Log.e("ActivityDC", "Unknown Error")
                 }
-                is Result.Failure -> Log.e("ActivityDC", result.error.reason)
-                else -> Log.e("ActivityDC", "Unknown Error")
             }
-        }
 
 
-        roomsListView.setOnItemClickListener { _: AdapterView<*>, _: View, i: Int, _: Long ->
-            if (currentRoom!=roomsList[i]) {
-                currentRoom = roomsList[i]
-                actionbar.title = currentRoom!!.name
-                supportFragmentManager.beginTransaction().detach(chatFragment).attach(chatFragment).commit()
+            roomsListView.setOnItemClickListener { _: AdapterView<*>, _: View, i: Int, _: Long ->
+                if (currentRoom != roomsList[i]) {
+                    currentRoom = roomsList[i]
+                    actionbar.title = currentRoom!!.name
+                    supportFragmentManager.beginTransaction().detach(chatFragment).attach(chatFragment).commit()
+                }
+                mDrawerLayout.closeDrawers()
             }
-            mDrawerLayout.closeDrawers()
         }
     }
 
